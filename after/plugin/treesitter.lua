13bead23929fd213b8a910068894485ac3465472
Vishnu Sreekumar <vishnu.srkmr@gmail.com>
2024-01-10 14:28:01 +0800

    Fix breaking change in module name

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,7 +1,7 @@
 require'nvim-treesitter.configs'.setup {
 	-- A list of parser names, or "all" (the four listed parsers should always be installed)
 	ensure_installed = { 
-		"help",
+		"vimdoc",
 		"go",
 		"terraform",
 		"latex" ,
